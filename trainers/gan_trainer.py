839e37b2f4984423590a478f1195b00e02ff96eb
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-17 19:36:04 +0100

    GAN trainer class is implemented

--- trainers/gan_trainer.py
+++ trainers/gan_trainer.py
@@ -1,7 +1,7 @@
 from base.base_train import BaseTrain
 from tqdm import tqdm
 import numpy as np
-
+import tensorflow as tf
 
 class GANTrainer(BaseTrain):
     def __init__(self, sess, model, data, config, logger):
@@ -13,7 +13,23 @@ class GANTrainer(BaseTrain):
        -loop on the number of iterations in the config and call the train step
        -add any summaries you want using the summary
         """
-        pass
+        loop = tqdm(range(self.config.num_iter_per_epoch))
+        gen_losses = []
+        disc_losses = []
+        for _ in loop:
+            gen_loss, disc_loss = self.train_step()
+            gen_losses.append(gen_loss)
+            disc_losses.append(disc_loss)
+        gen_loss = np.mean(gen_losses)
+        disc_loss = np.mean(disc_losses)
+        
+        cur_it = self.model.global_step_tensor.eval(self.sess)
+        summaries_dict = {
+            "gen_loss": gen_loss,
+            "disc_loss": disc_loss,
+        }
+        self.logger.summarize(cur_it, summaries_dict=summaries_dict)
+        self.model.save(self.sess)
 
     def train_step(self):
         """
@@ -21,5 +37,25 @@ class GANTrainer(BaseTrain):
        - run the tensorflow session
        - return any metrics you need to summarize
        """
-        pass
+       # Generate noise from normal distribution
+        noise = tf.random_normal([self.config.batch_size,self.config.noise_dim])
+        
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            generated_images = self.model.generator(noise, training=True)
+            real_output = self.model.discriminator(self.data, training=True)
+
+            gen_loss = self.model.generator_loss(generated_output=generated_images)
+            disc_loss = self.model.discriminator_loss(real_output=real_output,generated_output=generated_images)
+
+        gradients_of_generator = gen_tape.gradient(gen_loss, self.model.generator.variables)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, self.model.discriminator.variables)
+
+        self.model.generator_optimizer.apply_gradients(zip(gradients_of_generator,self.model.generator.variables))
+        self.model.discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator,self.model.discriminator.variables))
+
+        return gen_loss, disc_loss
+
+
+        
+
 
