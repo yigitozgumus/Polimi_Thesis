341ca2514392eda757ebc160750e0ed4462f040f
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-17 16:52:06 +0100

    convert sequential models to functional API

--- models/gan_model.py
+++ models/gan_model.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from base.base_model import BaseModel
 
 
+
 class GAN(BaseModel):
 
     def __init__(self, config):
@@ -15,38 +16,42 @@ class GAN(BaseModel):
         self.X = tf.placeholder(tf.float32, shape=[None]+ self.config.state_size)
 
         # Make the Generator model
-        self.generator = tf.keras.Sequential()
-        self.generator.add(tf.keras.layers.Dense(7*7*256, use_bias=False, input_shape=self.config.noise_dim))
-        self.generator.add(tf.keras.layers.BatchNormalization())
-        self.generator.add(tf.keras.layers.LeakyReLU())
-        self.generator.add(tf.keras.layers.Reshape(7,7,256))
+        inputs_g = tf.keras.layers.Input(shape=self.config.noise_dim)
+        x = tf.keras.layers.Dense(7*7*256, use_bias=False)(inputs_g)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.LeakyReLU()(x)
+        x = tf.keras.layers.Reshape((7, 7, 256))(x)
 
-        assert self.generator.output_shape == (None, 7, 7, 256)
-        self.generator.add(tf.keras.layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-        assert self.generator.output_shape == (None, 7, 7, 128)
-        self.generator.add(tf.keras.layers.BatchNormalization())
-        self.generator.add(tf.keras.layers.LeakyReLU())
+        assert x.get_shape().as_list() == [None, 7, 7, 256]
+        x = tf.keras.layers.Conv2DTranspose(
+            128, (5, 5), strides=(1, 1), padding='same', use_bias=False)(x)
+        assert x.get_shape().as_list() == [None, 7, 7, 128]
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.LeakyReLU()(x)
 
-        self.generator.add(tf.keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-        assert self.genertor.output_shape == (None, 14, 14, 64)
-        self.generator.add(tf.keras.layers.BatchNormalization())
-        self.generator.add(tf.keras.layers.LeakyReLU())
+        x = tf.keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False)(x)
+        assert x.get_shape().as_list() == [None, 14, 14, 64]
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.LeakyReLU()(x)
 
-        self.generator.add(tf.keras.layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-        assert self.generor.output_shape == (None, 28, 28, 1)
+        x = tf.keras.layers.Conv2DTranspose(1, (5, 5), strides=(
+            2, 2), padding='same', use_bias=False, activation='tanh')(x)
+        assert x.get_shape().as_list() == [None, 28, 28, 1]
+        self.generator = tf.keras.models.Model(inputs=inputs_g, outputs=x)
 
         # Make the discriminator model
-        self.discriminator = tf.keras.Sequential()
-        self.discriminator.add(tf.keras.layers.Conv2D(64, (5,5),strides=(2, 2),padding='same'))
-        self.discriminator.add(tf.keras.layers.LeakyReLU())
-        self.discriminator.add(tf.keras.layers.Dropout(0.3))
+        inputs_d = tf.keras.layers.Input(shape=self.config.state_size)
+        x = tf.keras.layers.Conv2D(32, (5,5),strides=(2, 2),padding='same')(inputs_d)
+        x = tf.keras.layers.LeakyReLU()(x)
+        x = tf.keras.layers.AveragePooling2D(pool_size=(2 ,2),padding='same')(x)
 
-        self.discriminator.add(tf.keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-        self.discriminator.add(tf.keras.layers.LeakyReLU())
-        self.discriminator.add(tf.keras.layers.Dropout(0.3))
+        x = tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same')(x)
+        x = tf.keras.layers.LeakyReLU()(x)
+        x = tf.keras.layers.Dropout(0.3)(x)
 
-        self.discriminator.add(tf.keras.layers.Flatten())
-        self.discriminator.add(tf.keras.layers.Dense(1))
+        x = tf.keras.layers.Flatten()(x)
+        x = tf.keras.layers.Dense(1)(x)
+        self.discriminator = tf.keras.models.Model(inputs=inputs_d,outputs=x)
 
         # Implementation of losses
         
