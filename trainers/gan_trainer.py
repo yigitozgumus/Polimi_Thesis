c562e489aebb38b46e33f47a737696ada70003a8
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-17 22:55:03 +0100

    back to dev

--- trainers/gan_trainer.py
+++ trainers/gan_trainer.py
@@ -18,23 +18,37 @@ class GANTrainer(BaseTrain):
         loop = tqdm(range(self.config.num_iter_per_epoch))
         gen_losses = []
         disc_losses = []
+        img_gens = []
+        img_discs = []
         for _ in loop:
-            gen_loss, disc_loss = self.train_step()
+            img_gen,img_disc, gen_loss, disc_loss = self.train_step()
             gen_losses.append(gen_loss)
             disc_losses.append(disc_loss)
+            img_gens.append(img_gen)
+            img_discs.append(img_disc)
+            
         gen_loss = tf.math.reduce_mean(gen_losses)
         disc_loss = tf.math.reduce_mean(disc_losses)
+        
+        tf.summary.scalar("Generator_Loss", gen_loss)
+        tf.summary.scalar("Discriminator_Loss", disc_loss)
+        
+        #x_image = tf.summary.image('FromNoise', tf.reshape(img_gens[-1], [-1, 28, 28, 1]), 4)
+        #x_image2 = tf.summary.image('FromGen', tf.reshape(img_disc[-1], [-1, 28, 28, 1]), 4)
+        
 #         current = self.sess.run(self.model.cur_epoch_tensor)
         cur_it = self.model.global_step_tensor.eval(self.sess)
         if (cur_it % self.config.show_steps == 0 or cur_it == 1):
                 print('Epoch {}: Generator Loss: {}, Discriminator Loss: {}'.format(
                     cur_it, gen_loss, disc_loss))
         
-        summaries_dict = {
-            "gen_loss": gen_loss,
-            "disc_loss": disc_loss,
-        }
-        self.logger.summarize(cur_it, summaries_dict=summaries_dict)
+#         summaries_dict = {
+#             "g_loss": gen_loss,
+#             "d_loss": disc_loss,
+#         }
+        summary = tf.summary.merge_all()
+
+        self.logger.summarize(cur_it, summaries=sum)
         self.model.save(self.sess)
 
     #@tf.contrib.eager.defun
@@ -48,11 +62,14 @@ class GANTrainer(BaseTrain):
         noise = tf.random_normal([self.config.batch_size,self.config.noise_dim])
         
         with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-            generated_images = self.model.generator(noise, training=True)
+            generated_image = self.model.generator(noise, training=True)
+            
             real_output = self.model.discriminator(self.data, training=True)
-
-            gen_loss = self.model.generator_loss(generated_output=generated_images)
-            disc_loss = self.model.discriminator_loss(real_output=real_output,generated_output=generated_images)
+            generated_output = self.model.discriminator(generated_image, training=True)
+            with tf.name_scope('Generator_Loss'):
+                gen_loss = self.model.generator_loss(generated_output)
+            with tf.name_scope('Discriminator_Loss'):    
+                disc_loss = self.model.discriminator_loss(real_output,generated_output)
 
         gradients_of_generator = gen_tape.gradient(gen_loss, self.model.generator.variables)
         gradients_of_discriminator = disc_tape.gradient(disc_loss, self.model.discriminator.variables)
@@ -60,7 +77,7 @@ class GANTrainer(BaseTrain):
         self.model.generator_optimizer.apply_gradients(zip(gradients_of_generator,self.model.generator.variables))
         self.model.discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator,self.model.discriminator.variables))
 
-        return gen_loss, disc_loss
+        return generated_image,generated_output,gen_loss, disc_loss
 
 
         
--- utils/logger.py
+++ utils/logger.py
@@ -12,7 +12,7 @@ class Logger:
         self.test_summary_writer = tf.summary.FileWriter(os.path.join(self.config.summary_dir, "test"))
 
     # it can summarize scalars and images.
-    def summarize(self, step, summarizer="train", scope="", summaries_dict=None):
+    def summarize(self, step, summarizer="train", scope="", summaries=None):
         """
         :param step: the step of the summary
         :param summarizer: use the train summary writer or the test one
@@ -23,22 +23,6 @@ class Logger:
         summary_writer = self.train_summary_writer if summarizer == "train" else self.test_summary_writer
         with tf.variable_scope(scope):
 
-            if summaries_dict is not None:
-                summary_list = []
-                for tag, value in summaries_dict.items():
-                    if tag not in self.summary_ops:
-                        if len(value.shape) <= 1:
-                            self.summary_placeholders[tag] = tf.placeholder('float32', value.shape, name=tag)
-                        else:
-                            self.summary_placeholders[tag] = tf.placeholder('float32', [None] + list(value.shape[1:]), name=tag)
-                        if len(value.shape) <= 1:
-                            self.summary_ops[tag] = tf.summary.scalar(tag, self.summary_placeholders[tag])
-                        else:
-                            self.summary_ops[tag] = tf.summary.image(tag, self.summary_placeholders[tag])
-
-                    summary_list.append(self.sess.run(self.summary_ops[tag], {self.summary_placeholders[tag]: value}))
-
-                for summary in summary_list:
-                    summary_writer.add_summary(summary, step)
-                summary_writer.flush()
+            summary_writer.add_summary(summaries, step)
+            summary_writer.flush()
 
