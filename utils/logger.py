d0795efd2b53e9fc141fc452d76db43fd0b45f05
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-17 19:40:40 +0100

    logger refactoring

--- utils/logger.py
+++ utils/logger.py
@@ -8,8 +8,7 @@ class Logger:
         self.config = config
         self.summary_placeholders = {}
         self.summary_ops = {}
-        self.train_summary_writer = tf.summary.FileWriter(os.path.join(self.config.summary_dir, "train"),
-                                                          self.sess.graph)
+        self.train_summary_writer = tf.summary.FileWriter(os.path.join(self.config.summary_dir, "train"),self.sess.graph)
         self.test_summary_writer = tf.summary.FileWriter(os.path.join(self.config.summary_dir, "test"))
 
     # it can summarize scalars and images.
