d4169aa717350cc7dd3abea7cd1288a99cfc425b
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-17 10:15:55 +0100

    generator and discriminator models are added

--- models/gan_model.py
+++ models/gan_model.py
@@ -1,22 +1,60 @@
 import tensorflow as tf
-
-
-
 from base.base_model import BaseModel
 
 
 class GAN(BaseModel):
 
     def __init__(self, config):
-        self.config = config
-        # init the global step
-        self.init_global_step()
-        # init the epoch counter
-        self.init_cur_epoch()
+        super(GAN, self).__init__(config)
+        self.build_model()
+        self.init_saver()
+
+    def build_model(self):
+        self.is_training = tf.placeholder(tf.bool)
+        #image placeholder
+        self.X = tf.placeholder(tf.float32, shape=[None]+ self.config.state_size)
+
+        # Make the Generator model
+        self.generator = tf.keras.Sequential()
+        self.generator.add(tf.keras.layers.Dense(7*7*256, use_bias=False, input_shape=self.config.noise_dim))
+        self.generator.add(tf.keras.layers.BatchNormalization())
+        self.generator.add(tf.keras.layers.LeakyReLU())
+        self.generator.add(tf.keras.layers.Reshape(7,7,256))
+
+        assert self.generator.output_shape == (None, 7, 7, 256)
+        self.generator.add(tf.keras.layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+        assert self.generator.output_shape == (None, 7, 7, 128)
+        self.generator.add(tf.keras.layers.BatchNormalization())
+        self.generator.add(tf.keras.layers.LeakyReLU())
+
+        self.generator.add(tf.keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+        assert self.genertor.output_shape == (None, 14, 14, 64)
+        self.generator.add(tf.keras.layers.BatchNormalization())
+        self.generator.add(tf.keras.layers.LeakyReLU())
+
+        self.generator.add(tf.keras.layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+        assert self.generor.output_shape == (None, 28, 28, 1)
+
+        # Make the discriminator model
+        self.discriminator = tf.keras.Sequential()
+        self.discriminator.add(tf.keras.layers.Conv2D(64, (5,5),strides=(2, 2),padding='same'))
+        self.discriminator.add(tf.keras.layers.LeakyReLU())
+        self.discriminator.add(tf.keras.layers.Dropout(0.3))
+
+        self.discriminator.add(tf.keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+        self.discriminator.add(tf.keras.layers.LeakyReLU())
+        self.discriminator.add(tf.keras.layers.Dropout(0.3))
+
+        self.discriminator.add(tf.keras.layers.Flatten())
+        self.discriminator.add(tf.keras.layers.Dense(1))
+
+        # Implementation of losses
+        
+
+
 
     def init_saver(self):
         self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
 
-    def build_model(self):
-        pass
+
 
