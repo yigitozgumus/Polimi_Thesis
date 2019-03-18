fab40d29361939c0ff4923484a32c5b6ee073fe4
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-18 15:46:24 +0100

    gan trainer bugfix

--- trainers/gan_trainer.py
+++ trainers/gan_trainer.py
@@ -47,7 +47,7 @@ class GANTrainer(BaseTrain):
         # Generate noise from normal distribution
         noise = tf.random_normal([self.config.batch_size,self.config.noise_dim])
         noise_gen = self.sess.run(noise)
-        image = self.sess.run(self.data)
+        image = self.sess.run(self.iterator.get_next())
         feed_dict = {self.model.noise_input: noise_gen, self.model.real_image_input: image}
         gen_loss, disc_loss = self.sess.run([self.model.train_gen,self.model.train_disc],feed_dict=feed_dict)
 
