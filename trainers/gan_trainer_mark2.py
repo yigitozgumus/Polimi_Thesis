1ac447f8d363850197ff388db3e8e9eaac0b5123
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-19 21:48:53 +0100

    GAN_mark2 configs update and trainer implementation

--- trainers/gan_trainer_mark2.py
+++ trainers/gan_trainer_mark2.py
@@ -13,13 +13,45 @@ class GANTrainer_mark2(BaseTrain):
        -loop on the number of iterations in the config and call the train step
        -add any summaries you want using the summary
         """
-        pass
+        loop = tqdm(range(self.config.num_iter_per_epoch))
+        gen_losses = []
+        disc_losses = []
+        summaries = []
+        cur_epoch = self.model.cur_epoch_tensor.eval(self.sess)
+        # Make the iterator initializor
+        iterator = self.iterator.make_initializable_iterator()
+        next_element = iterator.get_next()
+        self.sess.run(iterator.initializer)
+        for epoch in loop:
+            gen_loss, disc_loss, summary = self.train_step(next_element)
+            gen_losses.append(gen_loss)
+            disc_losses.append(disc_loss)
+            summaries.append(summary)
+        self.logger.summarize(cur_epoch, summaries=summaries)
+        gen_loss = tf.math.reduce_mean(gen_losses).eval(session=self.sess)
+        disc_loss = tf.math.reduce_mean(disc_losses).eval(session=self.sess)
 
-    def train_step(self):
+        if (cur_epoch % self.config.show_steps == 0 or cur_epoch == 1):
+                print('Epoch {}: Generator Loss: {}, Discriminator Loss: {}'.format(
+                    cur_epoch, gen_loss, disc_loss))
+
+        self.model.save(self.sess)
+
+    def train_step(self,image):
         """
        implement the logic of the train step
        - run the tensorflow session
        - return any metrics you need to summarize
        """
-        pass
+        noise = tf.random_normal(
+            [self.config.batch_size, self.config.noise_dim])
+        noise_gen = self.sess.run(noise)
+        image_eval = self.sess.run(image)
+        feed_dict = {self.model.noise_input: noise_gen,
+                     self.model.real_image_input: image_eval}
+        gen_loss, disc_loss, summary, _, _ = self.sess.run(
+            [self.model.gen_loss, self.model.disc_loss, self.model.summary, self.model.train_gen, self.model.train_disc ], feed_dict=feed_dict)
+
+        return gen_loss, disc_loss, summary,
+
 
