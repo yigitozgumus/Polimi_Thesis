ba20d0cf662fc9eae7c04f9072aebad927c618ac
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-19 11:51:59 +0100

    Eager Model Files are added

---
+++ mains/main_gan_eager.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+
+from utils.data_generator import DataGenerator
+from models.gan_model_eager import GAN_eager
+from trainers.gan_trainer_eager import GANTrainer_eager
+from utils.config import process_config
+from utils.logger import Logger
+from utils.utils import get_args
+from utils.dirs import create_dirs
+
+
+def main(config):
+    # capture the config path from the run arguments
+    # then process the json configuration file
+
+    # create the experiments dirs
+    create_dirs([config.summary_dir, config.checkpoint_dir])
+    # create tensorflow session
+    sess = tf.Session()
+    # create your data generator
+    data = DataGenerator(config)
+    # create an instance of the model you want
+    model = GAN_eager(config)
+    # create tensorboard logger
+    logger = Logger(sess, config)
+    # create trainer and pass all the previous components to it
+    trainer = GANTrainer_eager(sess, model, data.dataset, config, logger)
+    #load model if exists
+    model.load(sess)
+    # here you train your model
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main()
+
