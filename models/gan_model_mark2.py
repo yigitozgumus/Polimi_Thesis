7104405f6bd3ca47b8e6525345a9858982c908e9
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-19 21:43:46 +0100

    GAN_mark2 model implementation is complete

--- models/gan_model_mark2.py
+++ models/gan_model_mark2.py
@@ -1,5 +1,6 @@
 
 import tensorflow as tf
+
 from base.base_model import BaseModel
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Dropout
@@ -15,15 +16,12 @@ class GAN_mark2(BaseModel):
         self.init_saver()
 
     def build_model(self):
-        # Placeholders for the noise and the image
-        self.noise_input = tf.placeholder(tf.float32, shape=[None,self.config.noise_dim])
-        self.real_image_input = tf.placeholder(tf.float32, shape=[None] + self.config.image_dims)
 
         # Make the Generator Model
         with tf.name_scope("Generator"):
             # Keras Model Object is Defined
             model_g = Sequential()
-            model_g.add(Dense(128 * 7 * 7, activation="relu", input_shape=self.config.noise_dim))
+            model_g.add(Dense(128 * 7 * 7, activation="relu", input_shape=[self.config.noise_dim]))
             model_g.add(Reshape((7, 7, 128)))
             model_g.add(UpSampling2D())
             model_g.add(Conv2D(128, kernel_size=3,strides=(1, 1), padding='same'))
@@ -47,34 +45,98 @@ class GAN_mark2(BaseModel):
             # Keras Model object is defined
             model_d = Sequential()
             model_d.add(Conv2D(32, kernel_size=3, strides=(2, 2),
-                             input_shape=self.config.state_size, padding="same"))
+                             input_shape=self.config.image_dims, padding="same"))
             model_d.add(LeakyReLU(alpha=self.config.leakyReLU_alpha))
-            model_d.add(Dropout(self.config.dropout_rate))
+            model_d.add(Dropout(rate=self.config.dropout_rate))
             model_d.add(Conv2D(64, kernel_size=3, strides=(2, 2), padding="same"))
             model_d.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
             model_d.add(BatchNormalization(momentum=self.config.batch_momentum))
             model_d.add(LeakyReLU(alpha=self.config.leakyReLU_alpha))
-            model_d.add(Dropout(self.config.dropout_rate))
+            model_d.add(Dropout(rate=self.config.dropout_rate))
             model_d.add(Conv2D(128, kernel_size=3, strides=(2, 2), padding="same"))
             model_d.add(BatchNormalization(momentum=0.8))
             model_d.add(LeakyReLU(alpha=self.config.leakyReLU_alpha))
-            model_d.add(Dropout(self.config.dropout_rate))
+            model_d.add(Dropout(rate=self.config.dropout_rate))
             model_d.add(Conv2D(256, kernel_size=3, strides=(1, 1), padding="same"))
             model_d.add(BatchNormalization(momentum=0.8))
             model_d.add(LeakyReLU(alpha=self.config.leakyReLU_alpha))
-            model_d.add(Dropout(self.config.dropout_rate))
+            model_d.add(Dropout(rate=self.config.dropout_rate))
             model_d.add(Flatten())
             model_d.add(Dense(1, activation='sigmoid'))
 
             model_d.summary()
-            input_d = Input(shape=self.config.state_size)
+            input_d = Input(shape=self.config.image_dims)
             output_validity = model_d(input_d)
 
             self.discriminator = Model(input_d, output_validity)
 
-            
-            
+        # Train part of the Model
+        # Get the Noise and generate a batch of New Images
+        self.noise_input = tf.placeholder(tf.float32, shape=[None, self.config.noise_dim])
+        with tf.name_scope("Generated_Image"):
+            generated_images = self.generator(self.noise_input,training=True)
+        # Get the images from the batch
+        real_image_input = tf.placeholder(tf.float32, shape=[None] + self.config.image_dims)
+        # Feed the Real image to the Discriminator and obtain the result
+        with tf.name_scope("Real_From_Disc"):
+            real_img_vals = self.discriminator(real_image_input, training=True)
+        # Feed the Generated image to the Discriminator and obtain the result
+        with tf.name_scope("Gen_From_Disc"):
+            generated_img_vals = self.discriminator(generated_images,training=False)
+        # Define the loss function of Discriminator
+        with tf.name_scope("Discriminator_Loss"):
+            self.disc_loss = self.discriminator_loss(self.real_img_vals,self.generated_img_vals)
+        # Define the loss function of Generator
+        with tf.name_scope("Generator_Loss"):
+            self.gen_loss = self.generator_loss(self.generated_images)
 
+        # Initialize and Connect the Optimizers
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            self.gen_optimizer = tf.train.AdamOptimizer(
+                learning_rate =self.config.generator_l_rate,beta1=self.config.optimizer_adam_beta1)
+            self.disc_optimizer = tf.train.AdamOptimizer(
+                learning_rate=self.config.discriminator_l_rate,beta1=self.config.optimizer_adam_beta1)
+        
+        # Make the minimization of the Models
+        # If there needs to be proprocessing for the gradients before applying them, use
+        # Compute_gradients then apply_gradients
+        with tf.name_scope('SGD_Gen'):
+            self.train_gen = self.gen_optimizer.minimize(self.gen_loss)
+        with tf.name_scope('SGD_Disc'):
+            self.train_disc = self.disc_optimizer.minimize(self.disc_loss)
+
+        # Variable Saving for the Tensorboard
+        tf.summary.scalar("Generator_Loss", self.gen_loss)
+        tf.summary.scalar("Discriminator_Loss", self.disc_loss)
+        x_image = tf.summary.image('From_Noise', tf.reshape(
+            self.generated_images, [-1, 28, 28, 1]))
+        x_image2 = tf.summary.image('Real_Image', tf.reshape(
+            self.real_image_input, [-1, 28, 28, 1]))
+
+        # Histogram for the Discriminator Network
+        for i in range(0, 18):
+            with tf.name_scope('Disc_layer' + str(i)):
+                pesos = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+                tf.summary.histogram('pesos' + str(i), pesos[i])
+        
+        self.summary = tf.summary.merge_all()
+        
+
+    # Implementation of losses
+    def generator_loss(self, generated_output):
+        return tf.losses.sigmoid_cross_entropy(tf.ones_like(generated_output), generated_output)
+
+    def discriminator_loss(self, real_output, generated_output):
+        real_loss = tf.losses.sigmoid_cross_entropy(
+            multi_class_labels=tf.ones_like(real_output), logits=real_output)
+
+        generated_loss = tf.losses.sigmoid_cross_entropy(
+            multi_class_labels=tf.zeros_like(generated_output), logits=generated_output)
+
+        total_loss = (real_loss +  generated_loss)
+
+        return total_loss
 
 
     def init_saver(self):
