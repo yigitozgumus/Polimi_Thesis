b10737b2c19aca4e82084778c4d7c0c764d41870
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-20 10:35:28 +0100

    cleanup before the merge

--- show_summary.py
+++ show_summary.py
@@ -1,12 +1,13 @@
-import os
 import sys
 import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from utils.config import process_config
-from models.gan_model import GAN
+from models.gan_model_mark2 import GAN_mark2
 def main():
     config = process_config(sys.argv[1])
 
-    model = GAN(config)
+    model = GAN_mark2(config)
     print("Generator Network")
     model.generator.summary()
     print("Discriminator Network")
