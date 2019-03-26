1dea65ef2baaf0a5d331d841f7aa893b1a64eb75
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-26 20:55:34 +0100

    new model change for experiment

--- models/gan.py
+++ models/gan.py
@@ -9,7 +9,7 @@ class GAN(BaseModel):
         self.init_saver()
 
     def build_model(self):
-
+        tf.reset_default_graph()
         # Placeholders
         self.noise_input = tf.placeholder(
             tf.float32, shape=[None, self.config.noise_dim]
@@ -18,20 +18,21 @@ class GAN(BaseModel):
             tf.float32, shape=[None] + self.config.image_dims
         )
         self.random_vector_for_generation = tf.random_normal(
-            [self.config.num_example_imgs_to_generate, self.config.noise_dim])
+            [self.config.num_example_imgs_to_generate, self.config.noise_dim]
+        )
+        self.initializer = tf.truncated_normal_self.initializer(stddev=0.02)
         # Make the Generator model
         with tf.name_scope("Generator"):
             # Input layer creates the entry point to the model
             inputs_g = tf.keras.layers.Input(shape=[self.config.noise_dim])
             # Densely connected Neural Network layer with 12544 Neurons.
             x_g = tf.keras.layers.Dense(
-                7 * 7 * 256, 
-                use_bias=False,
-                kernel_initializer=tf.truncated_normal_initializer(
-                    mean=0.0,stddev=0.02))(inputs_g)
+                7 * 7 * 256, use_bias=False, kernel_initializer=self.initializer
+            )(inputs_g)
             # Normalize the output of the Layer
             x_g = tf.keras.layers.BatchNormalization(
-                momentum=self.config.batch_momentum)(x_g)
+                momentum=self.config.batch_momentum
+            )(x_g)
             # f(x) = alpha * x for x < 0, f(x) = x for x >= 0.
             x_g = tf.keras.layers.LeakyReLU(alpha=self.config.leakyReLU_alpha)(x_g)
             # Reshaping the output
@@ -43,12 +44,13 @@ class GAN(BaseModel):
                 (5, 5),
                 strides=(1, 1),
                 padding="same",
-                use_bias=False, 
-                kernel_initializer=tf.truncated_normal_initializer(
-                    mean=0.0, stddev=0.02))(x_g)
+                use_bias=False#,
+                #kernel_initializer=self.initializer,
+            )(x_g)
             assert x_g.get_shape().as_list() == [None, 7, 7, 128]
             x_g = tf.keras.layers.BatchNormalization(
-                momentum=self.config.batch_momentum)(x_g)
+                momentum=self.config.batch_momentum
+            )(x_g)
             x_g = tf.keras.layers.LeakyReLU(alpha=self.config.leakyReLU_alpha)(x_g)
 
             x_g = tf.keras.layers.Conv2DTranspose(
@@ -56,12 +58,13 @@ class GAN(BaseModel):
                 (5, 5),
                 strides=(2, 2),
                 padding="same",
-                use_bias=False, 
-                kernel_initializer=tf.truncated_normal_initializer(
-                    mean=0.0, stddev=0.02))(x_g)
+                use_bias=False#,
+                #kernel_initializer=self.initializer,
+            )(x_g)
             assert x_g.get_shape().as_list() == [None, 14, 14, 64]
             x_g = tf.keras.layers.BatchNormalization(
-                momentum=self.config.batch_momentum)(x_g)
+                momentum=self.config.batch_momentum
+            )(x_g)
             x_g = tf.keras.layers.LeakyReLU(alpha=self.config.leakyReLU_alpha)(x_g)
 
             x_g = tf.keras.layers.Conv2DTranspose(
@@ -70,9 +73,9 @@ class GAN(BaseModel):
                 strides=(2, 2),
                 padding="same",
                 use_bias=False,
-                activation="tanh",
-                kernel_initializer=tf.truncated_normal_initializer(
-                    mean=0.0, stddev=0.02))(x_g)
+                activation="tanh"#,
+                #kernel_initializer=self.initializer,
+            )(x_g)
             assert x_g.get_shape().as_list() == [None, 28, 28, 1]
             self.generator = tf.keras.models.Model(inputs=inputs_g, outputs=x_g)
 
@@ -83,25 +86,23 @@ class GAN(BaseModel):
                 32,
                 (5, 5),
                 strides=(2, 2),
-                padding="same", 
-                kernel_initializer=tf.truncated_normal_initializer(
-                    mean=0.0, stddev=0.02))(inputs_d)
+                padding="same"#,
+                #kernel_initializer=self.initializer,
+            )(inputs_d)
             x_d = tf.keras.layers.LeakyReLU(alpha=self.config.leakyReLU_alpha)(x_d)
             x_d = tf.keras.layers.Dropout(rate=self.config.dropout_rate)(x_d)
             x_d = tf.keras.layers.Conv2D(
                 64,
                 (5, 5),
                 strides=(2, 2),
-                padding="same", 
-                kernel_initializer=tf.truncated_normal_initializer(
-                    mean=0.0, stddev=0.02))(x_d)
+                padding="same"#,
+                #kernel_initializer=self.initializer,
+            )(x_d)
             x_d = tf.keras.layers.LeakyReLU(alpha=self.config.leakyReLU_alpha)(x_d)
             x_d = tf.keras.layers.Dropout(rate=self.config.dropout_rate)(x_d)
             x_d = tf.keras.layers.Flatten()(x_d)
             x_d = tf.keras.layers.Dense(1)(x_d)
-            self.discriminator = tf.keras.models.Model(
-                inputs=inputs_d,
-                outputs=x_d)
+            self.discriminator = tf.keras.models.Model(inputs=inputs_d, outputs=x_d)
         # Evaluations for the training
         generated_image = self.generator(self.noise_input, training=True)
         real_output = self.discriminator(self.real_image_input, training=True)
@@ -111,24 +112,23 @@ class GAN(BaseModel):
         # Losses of the training of Generator and Discriminator
         with tf.name_scope("Generator_Loss"):
             self.gen_loss = tf.losses.sigmoid_cross_entropy(
-                tf.ones_like(generated_output), generated_output)
+                tf.ones_like(generated_output), generated_output
+            )
         with tf.name_scope("Discriminator_Loss"):
             self.disc_real_loss = tf.losses.sigmoid_cross_entropy(
                 multi_class_labels=tf.ones_like(real_output), logits=real_output
             )
             self.disc_gen_loss = tf.losses.sigmoid_cross_entropy(
-                multi_class_labels=tf.zeros_like(
-                    generated_output),
-                logits=generated_output
+                multi_class_labels=tf.zeros_like(generated_output),
+                logits=generated_output,
             )
             self.total_disc_loss = self.disc_real_loss + self.disc_gen_loss
-
 
         # Store the loss values for the Tensorboard
         tf.summary.scalar("Generator_Loss", self.gen_loss)
         tf.summary.scalar("Discriminator_Real_Loss", self.disc_real_loss)
-        tf.summary.scalar("Discriminator_Gen_Loss",self.disc_gen_loss)
-        tf.summary.scalar("Discriminator_Total_Loss",self.total_disc_loss)
+        tf.summary.scalar("Discriminator_Gen_Loss", self.disc_gen_loss)
+        tf.summary.scalar("Discriminator_Total_Loss", self.total_disc_loss)
 
         x_image = tf.summary.image(
             "From_Noise", tf.reshape(generated_image, [-1, 28, 28, 1])
@@ -137,10 +137,7 @@ class GAN(BaseModel):
             "Real_Image", tf.reshape(self.real_image_input, [-1, 28, 28, 1])
         )
         with tf.name_scope("Generator_Progress"):
-            self.progress_images = self.generator(
-                self.noise_input,
-                training=False
-            )
+            self.progress_images = self.generator(self.noise_input, training=False)
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         # Initialization of Optimizers
         with tf.control_dependencies(update_ops):
@@ -157,16 +154,13 @@ class GAN(BaseModel):
             )
 
         with tf.name_scope("SGD_Generator"):
-            self.train_gen = self.generator_optimizer.minimize(
-                self.gen_loss
-            )
+            self.train_gen = self.generator_optimizer.minimize(self.gen_loss)
 
         for i in range(0, 10):
             with tf.name_scope("layer" + str(i)):
                 pesos = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
                 tf.summary.histogram("pesos" + str(i), pesos[i])
         self.summary = tf.summary.merge_all()
-
 
     def init_saver(self):
         self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
