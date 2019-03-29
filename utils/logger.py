1f7a61f487705fe882eabd40b7a526fd9b4d5add
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-03-29 12:45:32 +0100

    logger fix for training

--- utils/logger.py
+++ utils/logger.py
@@ -22,8 +22,6 @@ class Logger:
         """
         summary_writer = self.train_summary_writer if summarizer == "train" else self.test_summary_writer
         with tf.variable_scope(scope):
-            # Flatten the Summaries
-            summaries = [item for sublist in summaries for item in sublist]
             for summary in summaries:
                 summary_writer.add_summary(summary, step)
             summary_writer.flush()
