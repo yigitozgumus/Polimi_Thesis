39b8f9af0c5ed322ccae9716036737c8e1e99fa6
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-04-05 10:12:53 +0200

    ones and zeros array bugfix

--- trainers/gan_trainer_tf.py
+++ trainers/gan_trainer_tf.py
@@ -75,8 +75,10 @@ class GANTrainer_TF(BaseTrain):
         # Instance noise additions
         if self.config.include_noise:
             # If we want to add this is will add the noises
-            real_noise = np.random.normal(scale=sigma, size=[self.config.batch_size] + self.config.image_dims)
-            fake_noise = np.random.normal(scale=sigma, size=[self.config.batch_size] + self.config.image_dims)
+            real_noise = np.random.normal(scale=sigma,
+                                          size=[self.config.batch_size] + self.config.image_dims)
+            fake_noise = np.random.normal(scale=sigma,
+                                          size=[self.config.batch_size] + self.config.image_dims)
         else:
             # Otherwise we are just going to add zeros which will not break anything
             real_noise = np.zeros(([self.config.batch_size] + self.config.image_dims))
@@ -85,7 +87,7 @@ class GANTrainer_TF(BaseTrain):
         image_eval = self.sess.run(image)
         # Construct the Feed Dictionary
         # Train the Discriminator on both real and fake images
-        _,disc_loss,summary_disc = self.sess.run(
+        _, disc_loss,summary_disc = self.sess.run(
             [self.model.train_disc,
              self.model.total_disc_loss,
              self.model.summary_disc],
@@ -125,9 +127,10 @@ class GANTrainer_TF(BaseTrain):
         return gen_loss, disc_loss, summary_gan, summary_disc
 
     def generate_labels(self, soft_labels):
+
         if not soft_labels:
-            true_labels = np.ones_like((self.config.batch_size, 1))
-            generated_labels = np.zeros_like((self.config.batch_size, 1))
+            true_labels = np.ones((self.config.batch_size, 1))
+            generated_labels = np.zeros((self.config.batch_size, 1))
         else:
             true_labels = np.zeros((self.config.batch_size, 1)) + \
                           np.random.uniform(low=0.0, high=0.1, size=[self.config.batch_size, 1])
