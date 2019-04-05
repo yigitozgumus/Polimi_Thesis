7384167f818a14d500117e750b287ab659d2230d
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-04-05 19:02:17 +0200

    Control dependency location change

--- models/gan_tf.py
+++ models/gan_tf.py
@@ -97,16 +97,18 @@ class GAN_TF(BaseModel):
         # Discriminator Network Operations
         self.disc_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope="DCGAN/Discriminator")
         # Initialization of Optimizers
+        
+        with tf.control_dependencies(self.gen_update_ops):
+            self.train_gen = self.generator_optimizer.minimize(
+                self.gen_loss, global_step=self.global_step_tensor,
+                var_list=self.generator_vars
+            )
+        with tf.control_dependencies(self.disc_update_ops):
+            self.train_disc = self.discriminator_optimizer.minimize(
+                self.total_disc_loss, global_step=self.global_step_tensor,
+                var_list=self.discriminator_vars
+            )
 
-        self.train_gen = self.generator_optimizer.minimize(
-            self.gen_loss, global_step=self.global_step_tensor,
-            var_list=self.generator_vars
-        )
-
-        self.train_disc = self.discriminator_optimizer.minimize(
-            self.total_disc_loss, global_step=self.global_step_tensor,
-            var_list=self.discriminator_vars
-        )
 
         for i in range(0, 10):
             with tf.name_scope("layer" + str(i)):
