3c4ce93cb302d11c0abf43d2941c8dad9796cf0d
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-04-20 23:26:55 +0200

    labelling fix

--- trainers/bigan_trainer.py
+++ trainers/bigan_trainer.py
@@ -162,7 +162,7 @@ class BIGANTrainer(BaseTrain):
                 np.arange(len(true_labels)),
                 size=int(self.config.trainer.noise_probability * len(true_labels)),
             )
-            true_labels[flipped_idx] = 1 - true_labels[flipped_idx]
+            generated_labels[flipped_idx] = 1 - generated_labels[flipped_idx]
             true_labels = np.ones((self.config.data_loader.batch_size, 1)) - np.random.uniform(
                 low=0.0, high=0.1, size=[self.config.data_loader.batch_size, 1]
             )
@@ -170,7 +170,7 @@ class BIGANTrainer(BaseTrain):
                 np.arange(len(generated_labels)),
                 size=int(self.config.trainer.noise_probability * len(generated_labels)),
             )
-            generated_labels[flipped_idx] = 1 - generated_labels[flipped_idx]
+            true_labels[flipped_idx] = 1 - true_labels[flipped_idx]
 
         return true_labels, generated_labels
 
