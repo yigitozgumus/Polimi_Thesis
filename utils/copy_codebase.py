ef5b3317733d329c59b8b7227054c6d9b550f80e
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-05-17 23:50:20 +0200

    copy codebase function updated

--- utils/copy_codebase.py
+++ utils/copy_codebase.py
@@ -4,10 +4,11 @@ from shutil import copy
 def copy_codebase(config):
     # Get class name
     model_name = config.model.name.split(".")[0]
-    model_src_file = "models/{}.py".format(model_name)
+    image_size = config.data_loader.image_size
+    model_src_file = "models/{}/{}.py".format(image_size, model_name)
     trainer_name = config.trainer.name.split(".")[0]
     trainer_src_file = "trainers/{}.py".format(trainer_name)
-    config_file = "configs/{}.json".format(model_name)
+    config_file = "configs/{}/{}.json".format(image_size, model_name)
 
     copy(src=model_src_file, dst=config.log.codebase_dir)
     copy(src=trainer_src_file, dst=config.log.codebase_dir)
