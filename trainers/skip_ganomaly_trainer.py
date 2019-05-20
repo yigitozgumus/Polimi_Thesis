2fe87b61c2b648c7a740e8fbfad0d2d1fa28fbbc
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-05-20 08:13:25 +0200

    ensuring bugfree test run for skip ganomaly

--- trainers/skip_ganomaly_trainer.py
+++ trainers/skip_ganomaly_trainer.py
@@ -113,7 +113,14 @@ class SkipGANomalyTrainer(BaseTrain):
             test_batch, test_labels = self.sess.run([self.data.test_image, self.data.test_label])
             test_loop.refresh()  # to show immediately the update
             sleep(0.01)
-            feed_dict = {self.model.image_input: test_batch, self.model.is_training: False}
+            real_noise, fake_noise = self.generate_noise(False, cur_epoch)
+
+            feed_dict = {
+                self.model.image_input: test_batch,
+                self.model.real_noise: real_noise,
+                self.model.fake_noise: fake_noise,
+                self.model.is_training: False,
+            }
             scores += self.sess.run(self.model.anomaly_score, feed_dict=feed_dict).tolist()
             inference_time.append(time() - test_batch_begin)
             true_labels += test_labels.tolist()
