408f04fe43499bff95ce210e07134f91e51f7f27
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-05-23 17:37:23 +0200

    config files are renamed and copy codebase script is copied to bugfix

--- utils/copy_codebase_new.py
+++ utils/copy_codebase_new.py
@@ -8,7 +8,7 @@ def copy_codebase(config):
     model_src_file = "models/new/{}.py".format( model_name)
     trainer_name = config.trainer.name.split(".")[0]
     trainer_src_file = "trainers/{}.py".format(trainer_name)
-    config_file = "configs/{}/{}.json".format(image_size, model_name)
+    config_file = "configs/new/{}.json".format( model_name)
 
     copy(src=model_src_file, dst=config.log.codebase_dir)
     copy(src=trainer_src_file, dst=config.log.codebase_dir)
