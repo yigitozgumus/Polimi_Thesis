e75dea8ac2954aea98c34ac9c6a4ea9535397090
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-05-29 11:54:34 +0200

    additional ebgan changes

--- models/new/ebgan.py
+++ models/new/ebgan.py
@@ -15,9 +15,12 @@ class EBGAN(BaseModel):
         # Initializations
         # Kernel initialization for the convolutions
         # self.init_kernel = tf.random_normal_initializer(mean=0.0, stddev=0.02)
-        self.init_kernel = tf.contrib.layers.xavier_initializer(
-            uniform=False, seed=None, dtype=tf.float32
-        )
+        if self.config.trainer.init_type == "normal":
+            self.init_kernel = tf.random_normal_initializer(mean=0.0, stddev=0.02)
+        elif self.config.trainer.init_type == "xavier":
+            self.init_kernel = tf.contrib.layers.xavier_initializer(
+                uniform=False, seed=None, dtype=tf.float32
+            )
         # Placeholders
         self.is_training = tf.placeholder(tf.bool)
         self.image_input = tf.placeholder(
