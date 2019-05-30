db25f824581f631538bacacf55b4a4490c01ee77
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-05-30 11:29:25 +0200

    new alad experiment for the queue

--- run_multiple_experiments.sh
+++ run_multiple_experiments.sh
@@ -6,4 +6,4 @@ python run_on_titan_parameters_new.py -c configs/new/fanogan.json -e fanogan_mar
 #python run_on_titan.py -c configs/32/ganomaly.json -e ganomaly_new_lr
 #python run_on_titan.py -c configs/32/skip_ganomaly.json -e skip_ganomaly_new_lr
 python run_on_titan_new.py -c configs/new/encebgan.json -e encebgan_test
-
+python run_on_titan.py -c configs/32/alad.json -e alad_fl_lr
