686adcd6bff60e8bd974cf27c53eac8fc801c46f
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-06-06 05:30:39 +0200

    multi exp

--- run_multiple_experiments.sh
+++ run_multiple_experiments.sh
@@ -6,5 +6,5 @@ python run_on_titan_parameters.py -c configs/32/alad.json -e alad_32_new_lr_abla
 python run_on_titan_parameters.py -c configs/32/ganomaly.json -e ganomaly_32_new_lr_ablation
 python run_on_titan_parameters.py -c configs/32/skip_ganomaly.json -e skip_ganomaly_new_lr_ablation
 python run_on_titan_parameters.py -c configs/32/anogan.json -e anogan_new_lr_ablation
-
+python run_on_titan_new.py -c configs/new/sencebgan.json -e sencebgan_exp_res
 
