5290c017eb8d53e670b5cad98b1fb61ecf08f7b0
Yigit Ozgumus <yigitozgumus1@gmail.com>
2019-07-10 23:09:36 +0200

    updating gif script

--- scripts/create_gif.py
+++ scripts/create_gif.py
@@ -6,8 +6,9 @@ from utils.dirs import listdir_nohidden
 from utils.config import process_config
 
 
-def create_gif(folder_name, output_name):
+def create_gif(folder_name, output_name, num):
     files = listdir_nohidden(folder_name)
+    files = [x  for x in files if x.startswith(str(num))]
     num_epochs = len(files)
     images = []
     for filename in files:
@@ -19,11 +20,12 @@ def main():
     argparser = argparse.ArgumentParser(description=__doc__)
     argparser.add_argument("-e", "--experiment", metavar="C", help="Experiment Name")
     argparser.add_argument("-n", "--name", metavar="N", help="Name of the gif")
+    argparser.add_argument("-r", "--num", metavar="R", type=int, help="row number of experiment")
     args = argparser.parse_args()
 
     # config = process_config(args.config)
     folder = os.path.join("Experiments", args.experiment, "generated")
-    create_gif(folder, args.name)
+    create_gif(folder, args.name, args.num)
 
 
 if __name__ == "__main__":
